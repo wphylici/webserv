fb961572379dd4aae21d45861b5cd65663dced6e
Сергей <wphylici@student.21-school.com>
2021-06-23 09:32:48 +0300

    added explanatory comment

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: wphylici <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 22:30:19 by wphylici          #+#    #+#             */
-/*   Updated: 2021/06/23 09:18:48 by wphylici         ###   ########.fr       */
+/*   Updated: 2021/06/23 09:29:10 by wphylici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int main ()
 	int j = 0;
 
 	ParseConfig start;
-	start.ParseConf();
+	start.ParseConf(); // Метод который запускает весь парсер
 
-	std::cout << start.getServInfo()[i]->getValueLocPath()[j] << std::endl;
+	std::cout << start.getServInfo()[i]->getValueLocPath()[j] << std::endl;  // Пример обращения к полю location
 
 	for (int i = 0; i <= start.getPosServ(); i++)  // <--- Этот кусок кода использовать
 		delete start.getServInfo()[i];			   // при завершении всей программы
